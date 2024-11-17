75b2c77c787429fd6e1c49f5153a568c2f8ac676
MokotoK <mokotok@protonmail.com>
2024-11-17 21:34:43 +1000

    Few more exercises

---
+++ exercise-1.10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('t');
+        }
+        else if (c == '\b') {
+            putchar('b');
+        }
+        else {
+            putchar(c);
+            }
+    }
+    return 0;
+}
+
