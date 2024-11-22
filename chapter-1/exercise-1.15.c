26ac1f5c44367130d22ecc6372cfa38ece23b5f7
MokotoK <mokotok@protonmail.com>
2024-11-22 22:09:14 +1000

    Still doesnt solve the problem

--- chapter-1/exercise-1.15.c
+++ chapter-1/exercise-1.15.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 
-float tempreature_converter(int x);
+float celcius_converter(int x);
+float farenheit_converter(int x);
+
 
 int main() {
     int input, choice;
 
+    printf("Press 'f' to convert to Farenheit, 'c' for Celcius or CTRL+D to exit\n");
+
     while ((choice = getchar()) != EOF) {
-        printf("Press 'f' to convert to Farenheit, 'c' for Celcius or CTRL+D to exit\n");
         
         if (choice == 'c') {
             printf("Enter a Tempreature to convert\n");
-            input = getchar(); 
-            printf("%3.2f\n", tempreature_converter(input));
+            printf("%3.2f\n", celcius_converter((input=getchar())));
         }
         
+        else if (choice == 'f') {
+            printf("Enter a Tempreature to convert to Farenheit\n");
+            printf("%3.2f\n", farenheit_converter((input=getchar())));
+        }
 
-        
+        else {
+            printf("Not a valid option\n");
+        }
+    }
     return 0;
 }
 
-float tempreature_converter(int celcius) {
+float celcius_converter(int celcius) {
     float f;
 
     f = 9.0 / 5.0 * celcius + 32;
@@ -27,4 +36,10 @@ float tempreature_converter(int celcius) {
     return f;
 }
 
-    
+float farenheit_converter(int farenheit) {
+    float c;
+
+    c = 5.0 / 9.0 * (farenheit + 32);
+
+    return c;
+}    
