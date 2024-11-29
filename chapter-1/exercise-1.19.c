35499f8d11c9df5564bd3257bf4fe4b3bb4a9b59
mokotok <mokotok@protonmail.com>
2024-11-29 14:03:36 +1000

    conquered the reverse a string basic in c

--- chapter-1/exercise-1.19.c
+++ chapter-1/exercise-1.19.c
@@ -21,7 +21,7 @@ int main() {
 
 int my_getline(char s[], int limit) {
     int c, i;
-
+                                                                            
     for (i = 0; i < limit && (c = getchar()) != EOF && c != '\n'; ++i) {
          s[i] = c;
     }
@@ -36,61 +36,9 @@ int my_getline(char s[], int limit) {
 
 void reverse(int len, char to[], char from[]) {
     int j = 0;
-    
-    for (int i = len-1; i > 0; --i) {
-        from[i] = to[j];
-        ++j;
+
+    for (int i = len - 2; i >= 0; i--) {
+        to[j] = from[i];
+        j++;
     }
 }
-/*
-#include <stdio.h>
-
-#define MAXLINE 1010
-
-int my_getline(char line[], int maxline);
-void copy(char to[], char from[]);
-
-int main() {
-    int len;
-    int max;
-    char line[MAXLINE];
-    char longest[MAXLINE];
-
-    max = 0;
-    while ((len = my_getline(line, MAXLINE)) > 0) {
-        if (len > max) {
-            max = len;
-            copy(longest, line);
-        }
-    }
-
-    if (max > 0) {
-        printf("Number of chars: %d, - %s\n", max - 2, longest);
-    }
-    return 0;
-}
-
-int my_getline(char s[], int lim) {
-    int c, i;
-
-    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-        s[i] = c;
-    }
-
-    if (c == '\n') {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
-    return i;
-}
-
-void copy(char to[], char from[]) {
-    int i;
-
-    i = 0;
-    while ((to[i] = from[i]) != '\0') {
-        ++i;
-    }
-}
-*/
