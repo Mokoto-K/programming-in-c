65802be49962b9babf12dafa5c5d72a1c1c1b61e
mokotok <mokotok@protonmail.com>
2024-12-16 15:43:01 +1000

    conditionals

---
+++ chapter-3/exercise-3.1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int binsearch(int x, int v[], int n);
+
+int main() {
+    int x = 2;
+    int n = 10;
+    int v[] = {1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 ,10};
+
+    printf("%d\n", binsearch(x, v, n));
+    return 0;
+}
+
+int binseach(int x, int v[], int n) {
+
+    int low, mid, high;
+
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid])
+            high = mid - 1;
+        else if (x > v[mid])
+            low = mid + 1;
+        else 
+            return mid;
+    }
+    return -1;
+}
