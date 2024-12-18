89d0cb6e13de8aef5c5e18866f8f870afd1c6685
mokotok <mokotok@protonmail.com>
2024-12-18 20:58:47 +1000

    did it

--- chapter-3/exercise-3.3.c
+++ chapter-3/exercise-3.3.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
 #define MAX 2000
 
 void expand(char s1[], char s2[]);
 
 int main() {
-    char s1[] = "hello, can you type a-b for me real quick";
+    char s1[] = "hello, can you type a-z for me real quick, or 0-9, or A-G, or a-z0-9";
     char s2[MAX];
 
     expand(s1, s2);
@@ -16,56 +15,32 @@ int main() {
 
 void expand(char s1[], char s2[]) {
 
-    int i, j;
-    int length = strlen(s1);
+    int i, j = 0;
+    for (i = 0; s1[i] != '\0'; ++i) {
+        if (s1[i] == '-' && i > 0 && s1[i+1] != '\0') {
 
-    for (i = 0; i < length; ++i) {
-        if (i > 0 && i < (length - 1) && s1[i] == '-') {
             int prev = s1[i-1];
-            if (prev == 'a' || prev == 'A' || prev == 0) {
-                int next = s1[i+1];
-                if (next == 'z' || next == 'Z' || next == 9) {
-                    switch (prev) {
-                        
-                        case 'a':
-                            for (int k = 0; k < 26; ++k) {
-                                s2[j] = k + 97;
-                                j++;
-                            }
-                            break;
+            int next = s1[i+1];
+            
+            int cond1 = prev >= 'a' && next <= 'z';
+            int cond2 = prev >= 'A' && next <= 'Z';
+            int cond3 = prev >= '0' && next <= '9';
 
-                        case 'A':
-                            for (int k = 0; k < 26; ++k) {
-                                s2[j] = k + 65;
-                                j++;
-                            }
-                            break;
-
-                            
-                        case 0:
-                            for (int k = 0; k < 9; ++k) {
-                                s2[j] = k - '0';
-                                j++;
-                            }
-                            break;
-
-                        default:
-                            break;
-                    }
-                }
-                else {
-                    s2[i] = s2[j];
-                    j++;
+            if (cond1 || cond2 || cond3) {
+                for (int k = prev+1; k < next; k++) {
+                    s2[j++] = k;
                 }
             }
             else {
-                s2[i] = s2[j];
+                s2[j] = s1[i];
                 j++;
             }
         }
         else {
-            s2[i] = s2[j];
+            s2[j] = s1[i];
             j++;
         }
     }
+    s2[j] = '\0';
+
 }
