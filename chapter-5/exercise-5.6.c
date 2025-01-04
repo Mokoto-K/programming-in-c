dd63f0b6f358ff4470d752032bcf61e0697f7b04
mokotok <mokotok@protonmail.com>
2025-01-04 03:57:52 +0000

    Unhappy with the progress

--- chapter-5/exercise-5.6.c
+++ chapter-5/exercise-5.6.c
@@ -2,44 +2,56 @@
 #include <string.h>
 
 #define MAXLINES 5000
-
+#define GIGA 5000000
 char *lineptr[MAXLINES];
 
-int readlines(char *lineptr[], int nlines);
+int readlines(char *lineptr[], int nlines, char everything[]);
 void writelines(char *lineptr[], int nlines);
 
 void qsort(char *lineptr[], int left, int right);
 
 int main() {
     int nlines;
+    char everything[GIGA];
 
-    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    if ((nlines = readlines(lineptr, MAXLINES, everything)) >= 0) {
         qsort(lineptr, 0, nlines - 1);
         writelines(lineptr, nlines);
         return 0;
     }
     else {
-        printf("error: input too big to sort\n")
+        printf("error: input too big to sort\n");
     }
 }
 
 #define MAXLEN 1000
 
-int getline(char *, int);
-char *alloc(int);
+int mygetline(char *s, int lim) {
+    int c, i;
 
-int readlines(char *lineptr[], int maxlines) {
+    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i) 
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+int readlines(char *lineptr[], int maxlines, char everything[]) {
     int len, nlines;
     char *p, line[MAXLEN];
 
     nlines = 0;
-    while ((len = getline(line, MAXLEN)) > 0 )
-        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+    while ((len = mygetline(line, MAXLEN)) > 0 )
+        if (nlines >= maxlines || (p + len >= everything + GIGA)) 
             return -1;
         else {
             line[len-1] = '\0';
             strcpy(p, line);
             lineptr[nlines++] = p;
+            p += len;
         }
     return nlines;
 }
@@ -65,7 +77,7 @@ void qsort(char*v[], int left, int right) {
     qsort(v, last + 1, right);
 }
 
-void swap(char *v[i], int i, int j) {
+void swap(char *v[], int i, int j) {
     char *temp;
 
     temp = v[i];
