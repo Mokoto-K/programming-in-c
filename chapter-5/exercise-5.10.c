667b7188427a25cda01252184fd34b70ef7b6448
mokotok <mokotok@protonmail.com>
2025-01-14 02:26:07 +0000

    got nothing done

---
+++ chapter-5/exercise-5.10.c
@@ -0,0 +1,12 @@
+// Write the progream expr which evaluates a reverse polish expression from the cl
+// where each operator or operand is a separate arg7ument.
+// eg = expr 2 3 4 + * evals too 2 x (3+4)
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+
+
+    return 0;
+}
