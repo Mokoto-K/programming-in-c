b2ebe143bc8e35ac5de2b3d292a946509f1383d0
mokotok <mokotok@protonmail.com>
2025-01-20 19:53:40 +1000

    getting owned by C

--- chapter-5/entab.c
+++ chapter-5/entab.c
@@ -5,26 +5,31 @@
 #define TAB 4
 
 int main() {
-    char c, d;
-    int space_count, i, div= 0;
+    int c, spaces, colnum = 0;
 
     while ((c = getchar()) != EOF) {
-       
-        // if the input is a space
-        if (c == ' '){
-            // increment the space counter by one
-            space_count++;
-            // check if the next input is also a space 
-            if ((d = getchar()) == ' ') {
-                // increment again
-                space_count++;
-                // keep incrementing until the input is not a space 
-                for (; (d = getchar()) == ' '; space_count++) 
-                    div = space_count / TAB;
 
-
+        if (c == ' ')
+            ++spaces;
+        else if (c != '\n') {
+            // Deal with the tabs first 
+            while (spaces >=TAB) {
+                putchar('\t');
+                spaces -= TAB;
+                colnum += TAB;
             }
+            // Deal with the spaces 
+            while (spaces > 0) {
+                putchar(' ');
+                --spaces;
+                ++colnum;
+            }
+
+            putchar(c);
         }
-    }
+        else 
+            colnum = 0;
+        }
+            
     return 0;
 }
