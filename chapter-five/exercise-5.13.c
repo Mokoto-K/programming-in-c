4d233475de3879feb16b8e71f84ab4add0cbd280
mokotok <mokotok@protonmail.com>
2025-01-22 15:47:36 +1000

    Go backwards to go forwards.. sometimes..

--- chapter-five/exercise-5.13.c
+++ chapter-five/exercise-5.13.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
     char *pl[MAXSIZE];
 
     // Get all the lines and store then in the array of pointers 
-    while ((c = myline(*pl)) > 0)
+    while ((c = myline(*pl)) > 2)
         /*printf("seg 2 %d\n", c);*/
         ;
     // Wind the array back to point x elements back 
@@ -35,8 +35,9 @@ int myline(char *pl) {
     // While the input isnt EOF or a new line, continue to add the char to the string 
     while ((c = getchar()) != EOF && c != '\n'){
         line[i] = c;
+        pl[i] = c;
         i++;
-        printf("index: %d char %s\n", i, line);
+        printf("index: %d char %s\n", i, pl);
     }
 
     // If the input is a new line, add it and the 0 char to the end of the string 
@@ -45,7 +46,8 @@ int myline(char *pl) {
         line[i++] = '\0';
     }
     // increment the array of pointers by one and add the string to the array 
-    /**pl++ = *line;*/
+    /**pl = line;*/
+    /*printf("%s \n", pl);*/
     // return whatever it is c finished on.
     return i;
 }
