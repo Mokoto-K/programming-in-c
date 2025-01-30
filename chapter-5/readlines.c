74b046bbddf57e216b66102b7ae889e1727a5315
mokotok <mokotok@protonmail.com>
2025-01-30 21:21:53 +1000

    more pointers

--- chapter-5/readlines.c
+++ chapter-5/readlines.c
@@ -7,23 +7,17 @@ void readlines(char **, int);
 int mygetline(char *s, int n);
 
 int main(int argc, char *argv[]) {
-    char *lines[MAXLINES]; // makes me an array of 5000 pointers
-    char **ptr = lines; // is a pointer that points to the first pointer in 
+    char lines[MAXLINES][MAXLEN]; 
+    int c, i;
 
-    lines[0] = "hello";
-    lines[1] = "world";
+   // This section in main is essentially readlines, i spent too long here
+    // and it does what readlines would do, holy fuck this was hard.
+    for (i = 0; (mygetline(lines[i], MAXLEN)) != 0; i++)
+        ;   
+    
+    for (int j = 0; j < i; j++)
+        printf("%s\n", lines[j]);
 
-    for (int i = 2; i > 0; i--) {
-        printf("%s\n", *ptr);
-        ptr++;
-    }
-    /*for (int i = 0; i < MAXLINES; i++) {*/
-    /*    *lines[i] = i;*/
-    /*    printf("%d, %c\n", i, *lines[i]); */
-    /*}        */
-    /*mygetline(lines[0], MAXLEN);*/
-    /**/
-    /*printf("%s\n", lines[0]);*/
     return 0;
 }
 
@@ -31,7 +25,14 @@ int mygetline(char *s, int n) {
     int c, i;
 
     for (i = 0; i < n && (c = getchar()) != '\n'; i++)
-        *s++ = c;
+        s[i] = c;
     
-    return 1;
+    if (i > 1) {
+        /*s[i++] = c;*/ //adds the space
+        s[i] = '\0';
+        /*printf("%s\n", s);*/
+        return i;
+    }
+    else 
+        return 0;
 }
