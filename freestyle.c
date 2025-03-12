10cf664aa3d2dcfd0d02aabda4c0aba6ea9b405d
mokoto-k <mokotok@protonmail.com>
2025-03-12 20:15:04 +1000

    I may never get good at programming

---
+++ freestyle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// I dont know if i can use structs... i wanna though, kinda the whole reason
+// for doing this.
+struct listelement {
+    char *todo;
+};
+
+void addtodo();
+void deletetodo();
+void edittodo();
+
+int main() {
+    char **todos;
+
+    // While no EOF mans.
+        // For todo in todos print.
+        // Wait for command.
+    return 0;
+}
