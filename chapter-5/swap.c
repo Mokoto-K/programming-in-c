79cbb36f5f1da8c33dfe5c29991240ae78536961
mokotok <mokotok@protonmail.com>
2025-03-22 21:09:21 +1000

    scribbling around

--- chapter-5/swap.c
+++ chapter-5/swap.c
@@ -1,18 +1,19 @@
-#include <linux/limits.h>
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int *temp = a;
-    a = b;
-    b = temp;
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 };
 
 
 int main(int argc, char *argv[]) {
-    int a = 1;
-    int b = 2;
+    int x = 1;
+    int y = 2;
 
-    printf("a: %d, b: %d\n", a, b); 
-    swap(&a, &b);
-    printf("a: %d, b: %d\n", a, b); 
+    printf("x: %d, y: %d\n", x, y); 
+    swap(&x, &y);
+    printf("x: %d, y: %d\n", x, y); 
+
+    return 0;
 }
